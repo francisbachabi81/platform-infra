2e36f65d0e96d742c35e78506eb4a9fbe4d73116
Francis Bachabi <Francis.Bachabi81@Gmail.COM>
2025-10-20 17:06:41 -0400

    updates

--- stacks/core/outputs.tf
+++ stacks/core/outputs.tf
@@ -1,25 +1,27 @@
+# outputs.tf
+
 output "ids" {
   value = {
-    law  = azurerm_log_analytics_workspace.plane.id
-    appi = azurerm_application_insights.plane.id
-    rsv  = azurerm_recovery_services_vault.plane.id
+    law  = try(one(azurerm_log_analytics_workspace.plane[*].id), null)
+    appi = try(one(azurerm_application_insights.plane[*].id),   null)
+    rsv  = try(one(azurerm_recovery_services_vault.plane[*].id), null)
   }
 }
 
 output "names" {
   value = {
-    law  = azurerm_log_analytics_workspace.plane.name
-    appi = azurerm_application_insights.plane.name
-    rsv  = azurerm_recovery_services_vault.plane.name
+    law  = try(one(azurerm_log_analytics_workspace.plane[*].name), null)
+    appi = try(one(azurerm_application_insights.plane[*].name),     null)
+    rsv  = try(one(azurerm_recovery_services_vault.plane[*].name),  null)
   }
 }
 
 output "observability" {
   value = {
-    law_workspace_id         = azurerm_log_analytics_workspace.plane.id
-    appi_resource_id         = azurerm_application_insights.plane.id
-    appi_connection_string   = azurerm_application_insights.plane.connection_string
-    appi_instrumentation_key = azurerm_application_insights.plane.instrumentation_key
+    law_workspace_id         = try(one(azurerm_log_analytics_workspace.plane[*].id),                 null)
+    appi_resource_id         = try(one(azurerm_application_insights.plane[*].id),                    null)
+    appi_connection_string   = try(one(azurerm_application_insights.plane[*].connection_string),     null)
+    appi_instrumentation_key = try(one(azurerm_application_insights.plane[*].instrumentation_key),   null)
   }
   sensitive = true
 }
