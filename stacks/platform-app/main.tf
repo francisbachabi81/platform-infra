70c29c54ec36fc6198497bca4ea6f20c8dedf4ed
Francis Bachabi <Francis.Bachabi81@Gmail.COM>
2025-11-21 11:09:15 -0500

    Update main.tf

--- stacks/platform-app/main.tf
+++ stacks/platform-app/main.tf
@@ -2,7 +2,7 @@
 terraform {
   required_version = ">= 1.6.5"
   required_providers {
-    azurerm = { source = "hashicorp/azurerm", version = "~> 4.9.0" }
+    azurerm = { source = "hashicorp/azurerm", version = "~> 4.14.0" }
     random  = { source = "hashicorp/random",  version = "~> 3.6" }
   }
 }
